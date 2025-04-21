d5fb1432fd88c0f5514fc5acf4e27d291f0bf3a5
Rose Francis <rosennabs@gmail.com>
2025-04-21 11:16:16 -0600

    Implement separete Dockerfiles for frontend and backend

---
+++ backend/server.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors'; //install type definition for cors "npm install --save-dev @types/cors"
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from Express with TypeScript!' });
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
