b482e8eb44361281473cfb66b6b1aefa9c1faf21
Rose Francis <rosennabs@gmail.com>
2025-05-09 22:29:27 -0600

    Add text to overlay

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,15 +14,26 @@ export default function Home() {
   // }, []);
 
   return (
-    <div className="relative">
-      <img className="opacity-60 object-cover scale-145 py-42" src="/banner-1.jpg" alt="image of a corporate woman" />
+   
+    <div className="flex relative items-center">
+      <img className="w-full h-full opacity-60 object-cover scale-145 py-42" src="/banner-1.jpg" alt="image of a corporate woman" />
 
       {/* gradient overlay: dark → transparent, left → right */}
       <div className="absolute inset-0 bg-gradient-to-r from-black/50 via-black/20 to-transparent" />
 
       <Header />
+      <div className="absolute flex flex-col space-y-8 left-64 scale-125 w-[500px] z-20">
+        <h1 className="text-white text-7xl font-bold leading-20 tracking-tight">The Right Candidate for your Buisness</h1>
+        <p className="italic">There are many of passages of lorem Ipsum, but the majori have suffered alteration in some form.</p>
+        <button className="px-12 py-6 w-64 text-xs font-bold bg-green-600">
+          DISCOVER MORE
+        </button>
+      </div>
       
-    </div>
+      
+      </div>
+
+
     
   )
 }
