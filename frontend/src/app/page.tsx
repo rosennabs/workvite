f929fac4301528444d518c93aa78f69beea0cd05
Rose Francis <rosennabs@gmail.com>
2025-05-12 22:46:41 -0600

    Implement menuItem and update homepage styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,29 @@ import axios from 'axios';
 import Header from "@/components/Header";
 
 
+interface MenuItem {
+  icon: string,
+  heading: string,
+  image: string
+}
+const menuItems: MenuItem[] = [
+  {
+  icon: "/globe.svg",
+  heading: "Leadership Training",
+  image: "/feature-1.jpg"
+  },
+  {
+    icon: "/globe.svg",
+    heading: "Improving Resources",
+    image: "/feature-2.jpg"
+  },
+  {
+    icon: "/globe.svg",
+    heading: "Recruitement Solutions",
+    image: "/feature-3.jpg"
+  }
+]
+
 export default function Home() {
   // const [message, setMessage] = useState('');
 
@@ -14,26 +37,41 @@ export default function Home() {
   // }, []);
 
   return (
-   
-    <div className="flex relative items-center">
-      <img className="w-full h-full opacity-60 object-cover scale-145 py-42" src="/banner-1.jpg" alt="image of a corporate woman" />
+    <div className="flex flex-col items-center">
+    <div className="relative overflow-hidden">
+      <img className="w-full block opacity-60 object-cover scale-145 py-42" src="/banner-1.jpg" alt="image of a corporate woman" />
 
       {/* gradient overlay: dark → transparent, left → right */}
       <div className="absolute inset-0 bg-gradient-to-r from-black/50 via-black/20 to-transparent" />
 
-      <Header />
-      <div className="absolute flex flex-col space-y-8 left-64 scale-125 w-[500px] z-20">
+        <Header />
+        
+      <div className="absolute flex flex-col space-y-8 left-48 top-1/2 transform -translate-y-1/2 scale-125 w-[500px] z-20">
         <h1 className="text-white text-7xl font-bold leading-20 tracking-tight">The Right Candidate for your Buisness</h1>
         <p className="italic">There are many of passages of lorem Ipsum, but the majori have suffered alteration in some form.</p>
         <button className="px-12 py-6 w-64 text-xs font-bold bg-green-600">
           DISCOVER MORE
         </button>
-      </div>
-      
-      
+      </div> 
       </div>
 
+      <div className="bg-[#f6f6f6] text-black w-full min-h-[1000px] px-24">
 
+        <div className="grid grid-cols-3 gap-4 justify-items-center">
+          {menuItems.map((item, index) => (
+            <div key={index}
+              className="flex flex-col justify-center items-center bg-white w-[370px] h-[466px] p-8 space-y-8">
+              <img className="w-[65px]" src={item.icon} />
+              <h2 className="text-4xl font-bold text-center"> {item.heading} </h2>
+              <img src={item.image} />
+            </div>
+          ))}
+        
+        
+      </div>
+
+      </div>
+      </div>
     
   )
 }
