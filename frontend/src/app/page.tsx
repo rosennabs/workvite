ac75d96a0e0d2552ae7b7b4a456abeacd7aa29f2
Rose Francis <rosennabs@gmail.com>
2025-05-14 21:11:05 -0600

    Implement quotation box

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import Header from "@/components/Header";
+import { RiDoubleQuotesR } from "react-icons/ri";
 
 
 interface MenuItem {
@@ -55,9 +56,9 @@ export default function Home() {
       </div> 
       </div>
 
-      <div className="bg-[#f6f6f6] text-black w-full min-h-[1000px] px-24">
+      <div className="relative flex justify-center bg-[#f6f6f6] text-black w-full min-h-[586px] px-24">
 
-        <div className="grid grid-cols-3 gap-4 justify-items-center">
+        <div className="grid grid-cols-3 gap-6 justify-items-center">
           {menuItems.map((item, index) => (
             <div key={index}
               className="flex flex-col justify-center items-center bg-white w-[370px] h-[466px] p-8 space-y-8">
@@ -65,12 +66,25 @@ export default function Home() {
               <h2 className="text-4xl font-bold text-center"> {item.heading} </h2>
               <img src={item.image} />
             </div>
-          ))}
-        
-        
-      </div>
+          ))}        
+        </div>
+
+        <div className="absolute flex justify-center -bottom-15 z-10">
+          
+          <p className="relative border-2 border-green-600 bg-white italic text-xl py-8 pl-32 pr-8 w-[930px]">
+            Train people quickly well with quality business. So they highly efficient manufactured products. We can help you build.
+          </p>
+          <div className="absolute left-0 flex justify-center items-center text-3xl text-white bg-green-600 p-4 w-[65px] h-[60px]">
+            <RiDoubleQuotesR />
+          </div>
+        </div>
 
       </div>
+
+      <div className="bg-white w-full min-h-[1000px]">
+        <p>Another div</p>
+      </div>
+
       </div>
     
   )
