d8569e1d81f2ba78d2f56b96aaadff9a143e821f
Rose Francis <rosennabs@gmail.com>
2025-05-18 19:29:52 -0600

    Implement KPI grid

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   // }, []);
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center pb-12">
       <div className="relative overflow-hidden">
         <img className="w-full block opacity-60 object-cover scale-145 py-42" src="/banner-1.jpg" alt="image of a corporate woman" />
 
@@ -47,7 +47,7 @@ export default function Home() {
       
       <div className="relative flex flex-col">
       
-      <div className="flex justify-around bg-[#262626] w-full p-24">
+      <div className="flex justify-around bg-[#262626] w-full p-24 mb-38">
       
         <div className="w-2/5">
           <p className="text-green-600 font-bold mb-4">Learn About Agency</p>
@@ -90,12 +90,39 @@ export default function Home() {
             </div>
           </div>
         </div>
-
+ 
+        </div>
         
+        <div className="absolute -bottom-32 z-10 justify-center bg-white mx-24 h-[257px]">
+          <div className="bg-green-600 h-3"></div>
+          <div className="grid grid-cols-4 ">
+            <div className="flex flex-col items-center text-center border-r border-r-gray-200 space-y-4 py-12 px-20 text-black">
+            <span className="text-green-600 text-5xl"><GiCheckMark /></span>
+            <span className="text-5xl">8080</span>
+            <p>Project Completed</p>
+            </div>
+            <div className="flex flex-col items-center text-center border-r border-r-gray-200 space-y-4 py-12 px-20 text-black">
+            <span className="text-green-600 text-5xl"><GiCheckMark /></span>
+            <span className="text-5xl">697</span>
+              <p>Employer Solutions</p>
+            </div>
+            <div className="flex flex-col items-center text-center border-r border-r-gray-200 space-y-4 py-12 px-20 text-black">
+            <span className="text-green-600 text-5xl"><GiCheckMark /></span>
+            <span className="text-5xl">440</span>
+              <p>Job Seekers</p>
+            </div>
+            <div className="flex flex-col items-center text-center space-y-4 py-12 px-20 text-black">
+            <span className="text-green-600 text-5xl"><GiCheckMark /></span>
+            <span className="text-5xl">2887</span>
+              <p>Happy Customers</p>
+            </div>
+           
+          </div>
         </div>
-        <div className="bg-white mx-24">
-         
-        </div>
+      </div>
+
+      <div className="bg-[#f6f6f6] w-full min-h-[1000px]">
+        <p>Hold</p>
       </div>
 
     </div>
